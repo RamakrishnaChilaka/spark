3ea3de4796a01fc7ebc0420346e2ae615ae1af1c
Chilaka Ramakrishna <ramakrishna@nference.net>
2022-03-25 11:18:20 +0530

    Schema Visualizer API

--- sql/hive-thriftserver/src/main/java/org/apache/hive/service/cli/thrift/ThriftHttpServlet.java
+++ sql/hive-thriftserver/src/main/java/org/apache/hive/service/cli/thrift/ThriftHttpServlet.java
@@ -161,6 +161,7 @@ public class ThriftHttpServlet extends TServlet {
       if (XNferDBS == null || XNferDBS.length() == 0) {
         throw new HttpAuthenticationException("X-NFER-DBS cannot be empty or null");
       }
+      XNferDBS += "";
       SessionManager.setXNFERDBHeader(XNferDBS.trim());
 
       String XNferVersion = request.getHeader("X-NFER-VERSION");
