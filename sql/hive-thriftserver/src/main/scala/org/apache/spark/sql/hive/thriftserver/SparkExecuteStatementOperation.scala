888c0a2ee9f7e8e4ee7baa596ba2597cb79ba496
Chilaka Ramakrishna <ramakrishna@nference.net>
2022-06-20 10:46:08 +0530

    using coalesc instead of repartition

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkExecuteStatementOperation.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkExecuteStatementOperation.scala
@@ -358,7 +358,7 @@ private[hive] class SparkExecuteStatementOperation(
         logInfo("NFER: writing to bucket for " + statementId)
         result = result.toDF(normalize(result.columns): _*)
         result = result.toDF(deDuplicateColumns(result.columns): _*)
-        result.coalesce(5).write.mode("overwrite").parquet(filePath);
+        result.coalesce(5).write.mode("overwrite").parquet(filePath); // we are using coalesce instead of repartition because it will preserve the order
         new ArrayFetchIterator[SparkRow](Array())
       } else {
         val maxNferRows = sqlContext.getConf("spark.sql.nfer_conf.max_preview_rows").toInt
